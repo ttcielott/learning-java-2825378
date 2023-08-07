b5fef0586af80aba9c6ac5d26b97aba70af432ae
Haneul Kim <ttcielott@gmail.com>
2023-08-07 14:39:22 +0100

    practised

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double listedMealPrice,
+                                               double tipRate,
+                                               double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+
+        System.out.println("Your total mean price is " + result);
     }
 
     public static void main(String[] args) {
+        calculateTotalMealPrice(15.0, .2, .08);
+        calculateTotalMealPrice(15.0, .18, .08);
     }
 
 }
